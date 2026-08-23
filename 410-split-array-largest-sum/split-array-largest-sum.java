0d86af288a440efb48dc067a1f90b2624e6f4b0b
kashif-codes <razakashif4915@gmail.com>
2026-08-23 12:52:02 +0530

    Time: 1 ms (56.46%) | Memory: 43.2 MB (16.88%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end = 0;
+         
+         // find start = maximmum element inn array
+         // find end = sum of elements in array
+         for(int i = 0; i< nums.length;i++){
+            start = Math.max(start,nums[i]);
+            end += nums[i];
+        }
+
+        while(start <= end){
+            int mid = start+(end-start)/2;
+
+            if(canSplit(nums,k,mid)){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+
+    private boolean canSplit(int[] nums,int k,int maxSum){
+        int parts = 1; // we can start with one parts
+        int currSum = 0; // store the sum of current parts
+
+        for(int i = 0; i < nums.length; i++){
+            if(currSum + nums[i] > maxSum){  // if adding nums[i] to currSum make greater than maxSum
+                parts++;                    // then we need to add more parts
+                currSum = nums[i]; // store the new parts with nums[i]
+            }else{
+                // else nums[i] can fit in the current part
+                currSum += nums[i];
+            }
+        }
+        // if require parts are <= k,
+        //then max sum is possible
+        return parts <= k;
+
+    }
+}
