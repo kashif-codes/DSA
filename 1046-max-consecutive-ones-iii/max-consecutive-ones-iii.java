18c1c722720127bb20b2fcee5f312f52472f0c2c
kashif-codes <razakashif4915@gmail.com>
2026-09-15 19:28:37 +0530

    Time: 4 ms (44.57%) | Memory: 51.8 MB (96.43%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -4,22 +4,28 @@ class Solution {
         int maxLength = 0;
         int zeroesCount = 0;
 
-        // for right
+        // for right (traverse)
         for(int right = 0; right < nums.length; right++){
+            // if we find zero count it
             if(nums[right] == 0){
                 zeroesCount++;
             }
+            // if zero is more than k , shrink the window
             while(zeroesCount > k){
+                // if left element is zero , remove it from count
                 if(nums[left] == 0){
                     zeroesCount--;
                     
                 }
                 left++;
             }
+            // calculate current window length
             int currentLength = right - left + 1;
-
+           
+           // update max length
             maxLength = Math.max(maxLength , currentLength);
         }
+        // return max Length
         return maxLength;
     }
 }
