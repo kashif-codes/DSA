a896c425a85e88335ec22390e8d7cc237b77973d
kashif-codes <razakashif4915@gmail.com>
2026-09-18 20:54:53 +0530

    Time: 1 ms (96.26%) | Memory: 47.4 MB (59.99%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,28 +1,30 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        // initial right sum
+        // initial right sum 
         int rightSum = 0;
-        
-        // for right Sum 
         for(int i = 0; i < nums.length; i++){
-            // add current value to right sum
+            // add current element to right sum
             rightSum += nums[i];
         }
         // initial leftSum
         int leftSum = 0;
-
+        // check every index as possible
         for(int i = 0; i < nums.length; i++){
-            // right Sum - nums[i] = sum of element after i
+            // remove curent element from right side
+            // because right side only contain element after i
+            
             if(leftSum == rightSum - nums[i]){
                 return i;
+               
             }
-            // add current element from left
-            leftSum += nums[i];
-            // remove current element from right
+            // add current element to left side 
+             leftSum += nums[i];
+             // remove current element from right side
             rightSum -= nums[i];
         }
-        // if no pivot index is found
+        // if no valid index is found
         return -1;
+        
         
     }
 }
