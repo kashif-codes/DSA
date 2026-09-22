cd9191342e4214d7473e51256573eb080796cda0
kashif-codes <razakashif4915@gmail.com>
2026-09-22 13:22:10 +0530

    Time: 2 ms (97.70%) | Memory: 45.1 MB (40.83%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,33 +1,30 @@
-import java.util.HashSet;
+import java.util.*;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+       HashSet<Integer>set = new HashSet<>();
+       // store all element of nums1
+       for(int i = 0; i < nums1.length; i++){
+        set.add(nums1[i]);
+       }
+       HashSet<Integer>result = new HashSet<>();
+       // check which element of nums2 present in nums1
+       for(int i = 0; i < nums2.length; i++){
+        if(set.contains(nums2[i])){
+            result.add(nums2[i]);
+
+        }
+       }
+       // convert HashSet into int[]
+       // create an integer array with same size hashset
+       int[] ans = new int[result.size()];
+       int i = 0;
+       // take each element from hashset and store int the array
+       for(int num : result){
+        ans[i] = num;
+        i++;
+       }
+       return ans;
+
         
-        HashSet<Integer>set = new HashSet<>();
-        // put nums1 elments in set
-        for(int i = 0; i < nums1.length;i++){
-            set.add(nums1[i]);
-        }
-
-        HashSet<Integer>result = new HashSet<>();
-
-        // check nums2 element
-        for(int i = 0; i < nums2.length; i++){
-            if(set.contains(nums2[i])){
-                result.add(nums2[i]);
-            }
-        }
-
-        // convert result hashsey into int[]
-        int[] ans = new int[result.size()];
-        int i = 0;
-
-        Iterator<Integer>it = result.iterator();
-
-        for(int j = 0; j<result.size();j++){
-            ans[i] = it.next();
-            i++;
-        }
-        
-        return ans;
     }
 }
